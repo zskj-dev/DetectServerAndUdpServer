1423bcf3cde37e37fa83240be29f4ec0ba11b9f4
zhaobaoxin <1097267865@qq.com>
2025-12-03 20:04:07 +0800

    [COMMON]在数据库操作中，增加两个接口：1.id是否在指定表格中存在；2.获取表格中最大ID值

--- DetectServer/common/mysqloptor.py
+++ DetectServer/common/mysqloptor.py
@@ -106,6 +106,56 @@ class MysqlDB(object):
         except Exception as e:
             print("updateData 2 opt error:{}".format(e))
 
+    def checkIdExist(self, TableName, id, key='id'):
+        """
+        检查指定表中是否存在给定ID的记录
+
+        参数:
+            TableName (str): 数据库表名
+            id (int): 要检查的记录ID
+
+        返回:
+            bool: 如果记录存在返回True,否则返回False
+        """
+        try:
+            id = int(id)
+            table_name = str(TableName)
+            # self.conn.ping(reconnect=True)
+            # 添加调试信息
+            # print(f"DEBUG: table_name={table_name}, key={key}, id={id}")
+            sql = "SELECT * FROM {} WHERE {} = {}".format(table_name, key, id)
+            print(f"DEBUG: SQL={sql}")  # 打印生成的SQL
+            self.cur.execute(sql)
+            result = self.cur.fetchone()
+            return result is not None
+        except Exception as e:
+            print("check_id_exist opt error:{}".format(e))
+            return False
+
+    def getTalbeMaxID(self, TableName):
+        """
+        获取指定表中的最大ID值
+
+        参数:
+            TableName (str): 数据库表名
+
+        返回:
+            int: 表中的最大ID值，如果表为空则返回0
+        """
+        try:
+            table_name = str(TableName)
+            self.conn.ping(reconnect=True)
+            sql = "SELECT MAX(id) AS max_id FROM {}".format(table_name)
+            self.cur.execute(sql)
+            result = self.cur.fetchone()
+            if result and result['max_id'] is not None:
+                return result['max_id']
+            else:
+                return 0
+        except Exception as e:
+            print("get_table_max_id opt error:{}".format(e))
+            return 0    
+
 db = MysqlDB(MYSQL_HOST ,MYSQL_PORT, MYSQL_USER ,MYSQL_PWD ,MYSQL_DB)
 
 
